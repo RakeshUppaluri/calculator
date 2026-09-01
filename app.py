253d864c34b967571e4c2f489f895c0d14602da6
Rakesh <uppalurirakesh@gmail.com>
2026-09-01 15:44:53 +0530

    basic calculator

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title("Calculator Application")
+
+number1 = st.number_input("Insert a number",placeholder="Enter your first number")
+number2 = st.number_input("Insert a number",placeholder="Enter your second number")
+
+operation = st.selectbox("Select the operation",("Addition","Subtraction","Multiplication","Division"))
+
+ret=st.button("Calculate")
+if ret:
+    if operation=="Addition":
+        st.write(number1+number2)
+    elif operation=="Subtraction":
+        st.write(number1-number2)
+    elif operation=="Multiplication":
+        st.write(number1*number2)
+    elif operation=="Division":
+        st.write(number1/number2)
